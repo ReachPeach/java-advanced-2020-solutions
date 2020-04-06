d0b24c8ebcf59c9db7cd600673e98d5b0eece3f9
ReachPeach <maxim.busyuk@gmail.com>
2020-04-06 18:03:37 +0300

    fixes

--- java-solutions/ru/ifmo/rain/busyuk/implementor/generate-javadoc.cmd
+++ java-solutions/ru/ifmo/rain/busyuk/implementor/generate-javadoc.cmd
@@ -4,4 +4,4 @@ set res_out=./java-solutions/ru/ifmo/rain/busyuk/implementor/_javadoc
 set modulepath=../java-advanced-2020/lib/
 set sourcepath=./java-solutions/ru/ifmo/rain/busyuk/implementor/*.java
 
-javadoc %sourcepath -d %res_out -p %modulepath
+javadoc %sourcepath% -d %res_out% -p %modulepath% -private -link https://docs.oracle.com/en/java/javase/11/docs/api
