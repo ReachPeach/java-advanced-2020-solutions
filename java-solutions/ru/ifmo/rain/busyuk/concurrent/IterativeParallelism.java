105f355cc011f2b46ffd9938cb492e60faa2cd4f
ReachPeach <maxim.busyuk@gmail.com>
2020-04-14 13:03:57 +0300

    fixes

--- java-solutions/ru/ifmo/rain/busyuk/concurrent/IterativeParallelism.java
+++ java-solutions/ru/ifmo/rain/busyuk/concurrent/IterativeParallelism.java
@@ -7,9 +7,6 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class IterativeParallelism implements info.kgeorgiy.java.advanced.concurrent.AdvancedIP {
-    private int threadCount, blockCapacity, remaining;
-    private List<Thread> threads;
-
     @Override
     public <T> T maximum(int threads, List<? extends T> values, Comparator<? super T> comparator) throws InterruptedException {
         return minimum(threads, values, Collections.reverseOrder(comparator));
@@ -17,13 +14,13 @@ public class IterativeParallelism implements info.kgeorgiy.java.advanced.concurr
 
     @Override
     public <T> T minimum(int threads, List<? extends T> values, Comparator<? super T> comparator) throws InterruptedException {
-        return perform(threads, values, stream -> stream.min(comparator).orElseThrow(),
+        return mapReduce(threads, values, stream -> stream.min(comparator).orElseThrow(),
                 stream -> stream.min(comparator).orElseThrow());
     }
 
     @Override
     public <T> boolean all(int threads, List<? extends T> values, Predicate<? super T> predicate) throws InterruptedException {
-        return perform(threads, values, stream -> stream.allMatch(predicate), stream -> stream.allMatch(item -> item.equals(true)));
+        return mapReduce(threads, values, stream -> stream.allMatch(predicate), stream -> stream.allMatch(item -> item.equals(true)));
     }
 
     @Override
@@ -33,25 +30,25 @@ public class IterativeParallelism implements info.kgeorgiy.java.advanced.concurr
 
     @Override
     public String join(int threads, List<?> values) throws InterruptedException {
-        return perform(threads, values, stream -> stream.map(Object::toString).collect(Collectors.joining()),
+        return mapReduce(threads, values, stream -> stream.map(Object::toString).collect(Collectors.joining()),
                 stream -> stream.collect(Collectors.joining()));
     }
 
     @Override
     public <T> List<T> filter(int threads, List<? extends T> values, Predicate<? super T> predicate) throws InterruptedException {
-        return perform(threads, values, stream -> stream.filter(predicate).collect(Collectors.toList()),
+        return mapReduce(threads, values, stream -> stream.filter(predicate).collect(Collectors.toList()),
                 stream -> stream.flatMap(Collection::stream).collect(Collectors.toList()));
     }
 
     @Override
     public <T, U> List<U> map(int threads, List<? extends T> values, Function<? super T, ? extends U> f) throws InterruptedException {
-        return perform(threads, values, stream -> stream.map(f).collect(Collectors.toList()),
+        return mapReduce(threads, values, stream -> stream.map(f).collect(Collectors.toList()),
                 stream -> stream.flatMap(Collection::stream).collect(Collectors.toList()));
     }
 
     @Override
     public <T> T reduce(int threads, List<T> values, Monoid<T> monoid) throws InterruptedException {
-        return perform(threads, values, stream -> stream.reduce(monoid.getIdentity(), (left, right) ->
+        return mapReduce(threads, values, stream -> stream.reduce(monoid.getIdentity(), (left, right) ->
                         monoid.getOperator().apply(left, right), monoid.getOperator()),
                 stream -> stream.reduce(monoid.getIdentity(), (left, right) ->
                         monoid.getOperator().apply(left, right), monoid.getOperator()));
@@ -62,21 +59,17 @@ public class IterativeParallelism implements info.kgeorgiy.java.advanced.concurr
         return reduce(threads, map(threads, values, lift), monoid);
     }
 
-    private <T, R> R perform(int threadProvidedCount, List<? extends T> values, Function<Stream<? extends T>, R> sourceApplier,
-                             Function<Stream<R>, R> resultsApplier) throws InterruptedException {
-        threadCount = Math.min(threadProvidedCount, values.size());
-        threads = new ArrayList<>();
-        blockCapacity = values.size() / threadCount;
-        remaining = values.size() - threadCount * blockCapacity;
+    private <T, R> R mapReduce(int providedThreadCount, List<? extends T> values, Function<Stream<? extends T>, R> mapper,
+                               Function<Stream<R>, R> reducer) throws InterruptedException {
+        if (providedThreadCount <= 0 || values == null) {
+            throw new IllegalArgumentException("provided 0 threads or emphty values");
+        }
+        int threadCount = Math.min(providedThreadCount, values.size());
+        List<Thread> threads = new ArrayList<>();
+        int blockCapacity = values.size() / threadCount;
+        int remaining = values.size() % threadCount;
         List<R> threadsResults = new ArrayList<>();
         for (int i = 0; i < threadCount; i++) threadsResults.add(null);
-        fillThreads(threadsResults, values, sourceApplier);
-        joinThreads();
-        return resultsApplier.apply(threadsResults.stream());
-    }
-
-    private <T, R> void fillThreads(List<R> threadsResults, List<? extends T> values,
-                                    Function<Stream<? extends T>, R> sourceApplier) {
         for (int i = 0, l, r = 0; i < threadCount; i++) {
             l = r;
             r += blockCapacity;
@@ -85,18 +78,21 @@ public class IterativeParallelism implements info.kgeorgiy.java.advanced.concurr
                 remaining--;
             }
             if (l == r) break;
-            fillThread(threadsResults, values.subList(l, r).stream(), sourceApplier, i);
+            fillThread(threadsResults, values.subList(l, r).stream(), mapper, i, threads);
         }
+        joinThreads(threads);
+        return reducer.apply(threadsResults.stream());
     }
 
+
     private <T, R> void fillThread(List<R> threadsResult, Stream<? extends T> values,
-                                   Function<Stream<? extends T>, R> sourceApplier, int index) {
+                                   Function<Stream<? extends T>, R> sourceApplier, int index, List<Thread> threads) {
         Thread thread = new Thread(() -> threadsResult.set(index, sourceApplier.apply(values)));
         thread.start();
         threads.add(thread);
     }
 
-    private void joinThreads() throws InterruptedException {
+    private void joinThreads(List<Thread> threads) throws InterruptedException {
         List<InterruptedException> exceptions = new ArrayList<>();
         for (Thread thread : threads) {
             try {
