b585b4514ce18ee4c2ac030366e4fda2decbc7c3
ReachPeach <maxim.busyuk@gmail.com>
2020-06-16 11:04:28 +0300

    fixes

--- scripts/ru/ifmo/rain/busyuk/bank/runBankTests.bat
+++ scripts/ru/ifmo/rain/busyuk/bank/runBankTests.bat
@@ -2,5 +2,5 @@ cd %~dp0
 cd ..\..\..\..\..\..\
 set junit="lib\*"
 java -cp build;%junit% ru.ifmo.rain.busyuk.bank.tests.BankTests
-if errorlevel 1 exit \B 1
-exit \B 0
+if errorlevel 1 exit /B 1
+exit /B 0
